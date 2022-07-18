efbf37dd79f84ed91bc189a93f866aef319320ec
Felipe Parreira <felipeparreirafb@gmail.com>
2022-07-18 17:50:30 -0300

    test: ensure SignUpController returns 200 if valid data is passed

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -199,4 +199,25 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        email: 'invalid@email.com',
+        password: '123abc',
+        passwordConfirmation: '123abc'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid-id',
+      name: 'a name',
+      email: 'email@mail.com',
+      password: '123abc'
+    })
+  })
 })
