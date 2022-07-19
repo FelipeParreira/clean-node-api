1bd64c84000b868fa459e58efaced012b877f5e2
Felipe Parreira <felipeparreirafb@gmail.com>
2022-07-19 11:41:37 -0300

    refactor: extract sut to factory

--- src/data/usecases/addAccount/db-add-account.spec.ts
+++ src/data/usecases/addAccount/db-add-account.spec.ts
@@ -1,3 +1,4 @@
+import { Encrypter } from '../../protocols/encrypter'
 import { DbAddAccount } from './db-add-account-copy'
 
 class EncrypterStub {
@@ -6,10 +7,19 @@ class EncrypterStub {
   }
 }
 
+const makeSut = (): { sut: DbAddAccount, encrypterStub: Encrypter } => {
+  const encrypterStub = new EncrypterStub()
+  const sut = new DbAddAccount(encrypterStub)
+
+  return {
+    sut,
+    encrypterStub
+  }
+}
+
 describe('DbAddAccount Usecase', () => {
   test('should call Encrypter with the correct password', async () => {
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     await sut.add({
       name: 'a name',
