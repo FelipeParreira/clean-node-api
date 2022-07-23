19c9ea3eba22b70bc5207f9fdebb4a081242143d
Felipe Parreira <felipeparreirafb@gmail.com>
2022-07-22 22:40:23 -0300

    test: ensure bodyParser parses body as JSON

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('should parse body as JSON', async () => {
+    const path = '/test_body_parser'
+
+    app.post(path, (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post(path)
+      .send({ name: 'John' })
+      .expect({ name: 'John' })
+  })
+})
