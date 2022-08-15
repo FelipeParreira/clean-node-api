abf4f4366c7ddcdd0d9f721b2e3bd3b2b18d87ba
Felipe Parreira <felipeparreirafb@gmail.com>
2022-08-15 15:07:20 -0300

    refactor: move sut creation to a factory function

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -4,16 +4,22 @@ import { Validation } from './validation'
 
 class ValidationStub implements Validation {
   validate (input: any): Error | undefined {
-    return new MissingParamError('field')
+    return undefined
   }
+}
+
+const makeSut = (): { sut: ValidationComposite, validationStub: Validation} => {
+  const validationStub = new ValidationStub()
+  const sut = new ValidationComposite([validationStub])
+
+  return { sut, validationStub }
 }
 
 describe('Validation Composite', () => {
   test('should return the error returned by the fisrt validation that fails', () => {
+    const { sut, validationStub } = makeSut()
     const validationError = new MissingParamError('field')
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
-
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(validationError)
     const error = sut.validate({ field: 'value' })
 
     expect(error).toEqual(validationError)
