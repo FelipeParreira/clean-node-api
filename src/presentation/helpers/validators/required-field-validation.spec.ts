8d706395904d725c3094d53a17bf06b87bdbd002
Felipe Parreira <felipeparreirafb@gmail.com>
2022-08-15 14:53:56 -0300

    feat: ensure RequireFieldValidation returns undefined if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,13 +1,22 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '../../errors/missing-param-error'
 
+const field = 'field'
+
 describe('Required Field Validation', () => {
   test('should return a MissingParamError if validation fails', () => {
-    const field = 'field'
     const sut = new RequiredFieldValidation(field)
 
     const error = sut.validate({ name: 'some value' })
 
     expect(error).toEqual(new MissingParamError(field))
+  })
+
+  test('should return undefined if validation succeeds', () => {
+    const sut = new RequiredFieldValidation(field)
+
+    const response = sut.validate({ [field]: 'some value' })
+
+    expect(response).not.toBeDefined()
   })
 })
