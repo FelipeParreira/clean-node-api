d1dc8d42187858902fd1ac41d5949a6af315524d
Felipe Parreira <felipeparreirafb@gmail.com>
2022-08-16 17:05:46 -0300

    refactor: move duplicated code to factory functions

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,30 +1,45 @@
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { AccountModel } from '../add-account/db-add-account-protocols'
 import { DbAuthentication } from './db-authentication'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
+
+const makeAccount = (): AccountModel => ({
+  id: 'an id',
+  name: 'name',
+  email: 'mail@mail.com',
+  password: 'any password'
+})
+
+const makeAuthentication = (): AuthenticationModel => ({
+  email: 'email@mail.com',
+  password: '123abc'
+})
 
 class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
   async load (email: string): Promise<AccountModel> {
-    return {
-      id: 'an id',
-      name: 'name',
-      email: 'mail@mail.com',
-      password: 'any password'
-    }
+    return makeAccount()
+  }
+}
+
+const makeSut = (): { sut: DbAuthentication, loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository } => {
+  const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub
   }
 }
 
 describe('DbAuthentication UseCase', () => {
   test('should call LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    const email = 'mail@mail.com'
-    await sut.auth({
-      email,
-      password: '123abc'
-    })
+    const authentication = makeAuthentication()
+
+    await sut.auth(authentication)
 
     expect(loadSpy).toHaveBeenCalledTimes(1)
-    expect(loadSpy).toHaveBeenCalledWith(email)
+    expect(loadSpy).toHaveBeenCalledWith(authentication.email)
   })
 })
