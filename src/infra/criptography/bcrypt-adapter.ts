7028d1d93577fd2b866fac32dae510be7b42b910
Felipe Parreira <felipeparreirafb@gmail.com>
2022-08-16 20:37:36 -0300

    feat: add compare method to BCryptAdapter

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,12 +1,17 @@
 import { Hasher } from '../../data/protocols/cryptography/hasher'
 import bcrypt from 'bcrypt'
+import { HashComparer } from '../../data/protocols/cryptography/hash-comparer'
 
-export class BCryptAdapter implements Hasher {
+export class BCryptAdapter implements Hasher, HashComparer {
   constructor (
     private readonly salt: number
   ) {}
 
   async hash (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare (password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash)
   }
 }
