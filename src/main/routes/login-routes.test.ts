140f451d95e2deab2afcd3d1f22ca0ce6e6bf7d2
Felipe Parreira <felipeparreirafb@gmail.com>
2022-08-17 13:56:44 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import bcrypt from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,26 @@ describe('Login Routes', () => {
           email: 'john@mail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await bcrypt.hash('123', 12)
+      const data = {
+        name: 'John',
+        email: 'john@mail.com',
+        password
+      }
+      await accountCollection.insertOne(data)
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: data.email,
+          password: '123'
         })
         .expect(200)
     })
