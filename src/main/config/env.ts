2485157170e7aa0c629fae6c758687117ec02e4a
Felipe Parreira <felipe.parreira@voxy.com>
2022-08-22 11:57:09 -0300

    chore: change dev script to use docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,4 @@ export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://mongo:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'sd;lkfj908345'
-};
+}
