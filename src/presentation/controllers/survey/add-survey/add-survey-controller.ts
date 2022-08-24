e5daa034b1292ab2b95580d015a4db2ce62e20ed
Felipe Parreira <felipe.parreira@voxy.com>
2022-08-24 16:00:12 -0300

    feat: ensure AddSurveyController calls AddSurvey use case with the correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,19 +1,24 @@
 import { badRequest, ok } from '../../../helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation, AddSurvey } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest
+    const { question, answers } = body
 
     const error = this.validation.validate(body)
     if (error) {
       return badRequest(error)
     }
 
+    await this.addSurvey.add({
+      question, answers
+    })
     return ok('')
   }
 }
