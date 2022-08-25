52040a951684c88a317ac75faefe7e31a936b941
Felipe Parreira <felipe.parreira@voxy.com>
2022-08-25 11:33:22 -0300

    refactor: move AuthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,7 @@
-import { HttpRequest } from '../protocols'
+import { AccountModel, LoadAccountByToken, HttpRequest } from './auth-middleware-protocols'
 import { AuthMiddleware } from './auth-middleware'
 import { forbidden, ok, serverError } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
-import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
-import { AccountModel } from '../../domain/models/account'
 
 const makeAccount = (): AccountModel => ({
   id: 'an id',
