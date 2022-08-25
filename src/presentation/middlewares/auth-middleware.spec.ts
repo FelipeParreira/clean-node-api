824b09c3efe1c1f1604a3497d76b1f9be738a01b
Felipe Parreira <felipe.parreira@voxy.com>
2022-08-25 11:06:45 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with the correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,10 +2,26 @@ import { HttpRequest } from '../protocols'
 import { AuthMiddleware } from './auth-middleware'
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
 
-const makeSut = (): { sut: AuthMiddleware } => {
-  const sut = new AuthMiddleware()
-  return { sut }
+const makeAccount = (): AccountModel => ({
+  id: 'an id',
+  name: 'name',
+  email: 'my_email@mail.com',
+  password: 'any hashed password'
+})
+
+class LoadAccountByTokenStub implements LoadAccountByToken {
+  async loadByToken (accessToken: string, role?: string | undefined): Promise<AccountModel> {
+    return makeAccount()
+  }
+}
+
+const makeSut = (): { sut: AuthMiddleware, loadAccountByTokenStub: LoadAccountByToken } => {
+  const loadAccountByTokenStub = new LoadAccountByTokenStub()
+  const sut = new AuthMiddleware(loadAccountByTokenStub)
+  return { sut, loadAccountByTokenStub }
 }
 
 const makeHttpRequest = (headers = {}): HttpRequest => ({
@@ -20,5 +36,16 @@ describe('Auth Middleware', () => {
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('should call LoadAccountByToken with the correct access token', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const httpRequest = makeHttpRequest({ 'x-access-token': '123abc' })
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'loadByToken')
+
+    await sut.handle(httpRequest)
+
+    expect(loadSpy).toHaveBeenCalledTimes(1)
+    expect(loadSpy).toHaveBeenCalledWith(httpRequest.headers['x-access-token'])
   })
 })
