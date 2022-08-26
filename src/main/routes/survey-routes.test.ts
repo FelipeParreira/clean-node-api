b3d603ae92fc13027edb1639f043ea0b6f7b6ec6
Felipe Parreira <felipe.parreira@voxy.com>
2022-08-26 13:27:30 -0300

    test: ensure add surveys is not accessible by non-admins

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -38,6 +38,31 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
+    test('should return 403 if a non-admin requests it', async () => {
+      const account = await accountCollection.insertOne({
+        name: 'John',
+        email: 'john@mail.com',
+        password: '123abc'
+      })
+      const id = account.insertedId
+      const accessToken = jwt.sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, { $set: { accessToken } })
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'a question?',
+          answers: [
+            { answer: 'answer1', image: 'http://image.com' },
+            { answer: 'answer2' }
+          ]
+        })
+        .expect(403)
+    })
+
     test('should return 204 on success', async () => {
       const account = await accountCollection.insertOne({
         name: 'John',
